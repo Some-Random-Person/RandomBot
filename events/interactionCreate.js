d2e4242aa0eaccf551ba21df8e7657c24e868a67
Some Random Person <thomas280703@gmail.com>
2024-11-04 14:34:44 +0100

    complete cooldown logic

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -31,11 +31,14 @@ module.exports = {
       if (now < expirationTime) {
         const expiredTimestamp = Math.round(expirationTime / 1_000);
         return interaction.reply({
-          context: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
+          content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
           ephemeral: true,
         });
       }
     }
+
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
     try {
       await command.execute(interaction);
