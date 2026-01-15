7438bee916cec27b2cd31784dcf6be9a3694332d
Some-Random-Person <frown-nutty-cosmos@duck.com>
2026-01-15 14:35:21 +0100

    removed old service files and routes, added jest

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
   {
@@ -14,6 +15,21 @@ export default defineConfig([
       camelcase: "error",
       curly: "error",
       eqeqeq: "error",
+    },
+  },
+  {
+    // jest
+    files: ["**/*.spec.js", "**/*.test.js"],
+    plugins: { jest: jest },
+    languageOptions: {
+      globals: jest.environments.globals.globals,
+    },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     },
   },
 ]);
